1bac0116d0205ff504b27aef6f0a2554c3c8d913
KeyeSko <aygiztaipoav@gmail.com>
2023-04-09 23:18:02 +0300

    django without auth

---
+++ Web-project/web-app/anecdotator/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def family(request):
+    return render(request, 'main/family.html')
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
