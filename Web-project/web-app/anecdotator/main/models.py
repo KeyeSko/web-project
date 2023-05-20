e2ca5908f8a1ac17e08a16f84ee7c637dc6f6196
KeyeSko <aygiztaipoav@gmail.com>
2023-05-20 20:38:44 +0300

    Anecdot model

--- Web-project/web-app/anecdotator/main/models.py
+++ Web-project/web-app/anecdotator/main/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 # Create your models here.
 
+class Anecdot(models.Model):
+
+    class AnecCategory(models.TextChoices):
+        Fam = "Family", "FAMILY"
+        Army = "Army", "ARMY"
+        Other = "Other", "OTHER"
+
+    text = models.TextField()
+    like_count = models.IntegerField(default='0')
+    category = models.CharField(max_length=10, choices=AnecCategory.choices, default='Other')
